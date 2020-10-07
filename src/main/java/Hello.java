9c2c21eec345a2f2a577b8b6d04233f6a9bccd0d
Dusmamat Karimov <dkarimov@gmail.com>
2020-10-07 21:05:05 +0100

    EU 3 doing great

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -3,5 +3,12 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello Class EU3");
         // more changes coming and i am ready to commit
+
+        // let us make make more change it is awesome
+
+        // EU is learning
+        // this is my first project
+         // let us have more change
+
     }
 }
